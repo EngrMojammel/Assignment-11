07b73882dc3521002793f0190c90eb0473481817
Mohammad Mojammel <135323951+EngrMojammel@users.noreply.github.com>
2023-08-18 23:54:40 +0600

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <h1>This is home page. Please go to the Blog page.</h1>
+    </div>
+  );
+};
+
+export default page;
