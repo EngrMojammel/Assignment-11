b4f8955403d5f02884009932f53e5102a72d216d
Mohammad Mojammel <135323951+EngrMojammel@users.noreply.github.com>
2023-08-18 23:58:06 +0600

    Add files via upload

---
+++ app/post/[id]/page.js
@@ -0,0 +1,24 @@
+'use client'
+import React from 'react';
+import blogPostsData from '../../data/blogPosts.json'
+
+
+const page = ({params: {id,string}}) => {
+ 
+const post = blogPostsData.find(p => p.id.toString() === id)
+    return (
+        <div>
+            <h1>{post.title}</h1>
+            <p>Date: {post.date}</p>
+            <div>{post.content}</div>
+        </div>
+    );
+};
+
+export async function generateStaticParams(){
+    return blogPostsData.map((p)=>({
+        id: p.id.toString(),
+    }))
+}
+
+export default page;
